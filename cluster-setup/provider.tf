cee7e702807ef650fe8a73ad4fe7a599073a52bf
Mayank Thakur <54767390+Mynkthkr@users.noreply.github.com>
2023-05-09 13:04:38 +0530

    Update provider.tf

--- cluster-setup/provider.tf
+++ cluster-setup/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAW54MKN5E3FSULS36"
-  secret_key = "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"
+  access_key = "6"
+  secret_key = "F0F"
 }
